3a24a4acfd52235c7af80ed129f106e2c74e5021
jkkitchen <jessica.k.kitchen@gmail.com>
2025-04-10 22:24:57 -0400

    Finished - have a couple of lighthouse errors

--- project/scripts/local-resources.js
+++ project/scripts/local-resources.js
@@ -3,7 +3,7 @@
 const fabricShops = [
     {
         shopName: "Marietta's Quilt & Sew",
-        location: "Simpsonville, SC",
+        location: "Simpsonville",
         services: "Longarm Quilting, Fabric, Classes, Notions, Janome and Babylock sewing machine maintenance and sales",
         fabricBrands: "Art Gallery Fabrics, Kimberbell Fabrics, Moda Fabrics, Riley Blake Designs, Robert Kaufman Fabrics, Ruby Star Society, Tula Pink",
         Image: "images/mariettas-quilt-shop.webp",
@@ -11,7 +11,7 @@ const fabricShops = [
     },
     {
         shopName: "We're in Stitches",
-        location: "Greenville, SC",
+        location: "Greenville",
         services: "Classes, Fabric, Notions, Bernina sewing machine maintenance and sales",
         fabricBrands: "Art Gallery Fabrics, Moda Fabrics, Ruby Star Society, Tula Pink",
         Image: "images/were-in-stitches.webp",
@@ -19,7 +19,7 @@ const fabricShops = [
     },
     {
         shopName: "Carolina Quilt Studio",
-        location: "Greenville, SC",
+        location: "Greenville",
         services: "Longarm Quilting, Classes, Fabric, Notions, Brother sewing machine maintenance and sales",
         fabricBrands: "Art Gallery Fabrics, Moda Fabrics, Free Spirit Fabrics, Liberty Fabrics",
         Image: "images/carolina-quilt-studio.webp",
@@ -27,7 +27,7 @@ const fabricShops = [
     },
     {
         shopName: "Hobby Lobby",
-        location: "Multiple Locations",
+        location: "Greenville, Taylors, Easley",
         services: "Fabric, Notions",
         fabricBrands: "Kona Solids",
         Image: "images/hobby-lobby.webp",
@@ -35,7 +35,7 @@ const fabricShops = [
     },
     {
         shopName: "All About Fabrics",
-        location: "Williamston, SC",
+        location: "Williamston",
         services: "Fabric, Notions",
         fabricBrands: "Free Spirit Fabrics, Bernatex",
         Image: "images/all-about-fabrics.webp",
@@ -43,8 +43,56 @@ const fabricShops = [
     }
 ]
 
+//Define new arrays for each location
+const shopsInGreenvilleArea = fabricShops;
+const shopsInGreenville = fabricShops.filter(shop => shop.location.includes("Greenville"));
+const shopsInSimpsonville = fabricShops.filter(shop => shop.location.includes("Simpsonville"));
+const shopsInTaylors = fabricShops.filter(shop => shop.location.includes("Taylors"));
+const shopsInEasley = fabricShops.filter(shop => shop.location.includes("Easley"));
+const shopsInWilliamston = fabricShops.filter(shop => shop.location.includes("Williamston"));
+
+
+//Create variables for the links
+const greenvilleAreaLink = document.querySelector("#greenville-area");
+const greenvilleLink = document.querySelector("#greenville");
+const simpsonvilleLink = document.querySelector("#simpsonville")
+const taylorsLink = document.querySelector("#taylors");
+const easleyLink = document.querySelector("#easley");
+const williamstonLink = document.querySelector("#williamston");
+
+//Add Event Listeners and create "fabric shop cards" for the requested area
+greenvilleAreaLink.addEventListener("click", () => {
+    createFabricShopCard(shopsInGreenvilleArea);
+});
+
+greenvilleLink.addEventListener("click", () => {
+    createFabricShopCard(shopsInGreenville);
+});
+
+simpsonvilleLink.addEventListener("click", () => {
+    createFabricShopCard(shopsInSimpsonville);
+});
+
+taylorsLink.addEventListener("click", () => {
+    createFabricShopCard(shopsInTaylors);
+});
+
+easleyLink.addEventListener("click", () => {
+    createFabricShopCard(shopsInEasley);
+});
+
+williamstonLink.addEventListener("click", () => {
+    createFabricShopCard(shopsInWilliamston);
+});
+
+//Default, or Home: Loop through the array and create "fabric shop cards" for each fabric shop
+createFabricShopCard(fabricShops);
+
 //Function to create squares for each fabric shop
 function createFabricShopCard(fabricShops) {
+    //Clear before each button is pushed so new arrays can be made
+    document.querySelector(".local-fabric-shops").innerHTML = '';
+    //Function
     fabricShops.forEach(shop => {
         let card = document.createElement("section");
         let name = document.createElement("h3");
@@ -66,8 +114,8 @@ function createFabricShopCard(fabricShops) {
         img.setAttribute("src", shop.Image);
         img.setAttribute("alt", `${shop.shopName}`);
         img.setAttribute("loading", "lazy");
-        img.setAttribute("width", "300");
-        img.setAttribute("height", "auto");
+        // img.setAttribute("width", "auto");
+        // img.setAttribute("height", "auto");
 
         //Append Elements to the "card"
         card.appendChild(name);
@@ -85,5 +133,4 @@ function createFabricShopCard(fabricShops) {
     });
 }
 
-//Default, or Home: Loop through the array and create "temple cards" for each temple
-createFabricShopCard(fabricShops);
+
